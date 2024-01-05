4c1cfa06977b1f740cdf0e2edc47cf3eaf4d55cd
Nicholas Rose <nick@Nicholass-MacBook-Air.local>
2024-01-04 20:27:45 -0500

    User signup and login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const expiresIn = {expiresIN: "1 day"};
+const expiresIn = {expiresIn: "1 day"};
 const SECRET = process.env.JWT;
 
 //! User Signup
@@ -33,7 +33,34 @@ router.post('/signup', async(req, res) => {
     }
 });
 //!User Login
-
+router.post('/login', async(req, res) => {
+    try {
+        
+        //1. Capture data provided by user (req.body)
+        const { email, password } = req.body; // this requests the email and password from the body request.
+        //2. Check database to see if email supplied email exists
+        const user = await User.findOne({email: email}); //* a mongoDB method that accepts a query as an argument. Returns an instance of a document that matches
+        // console.log(user);
+        if(!user) throw new Error('Email or Password does not match');
+        //3. If email exists, consider if the password matches
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        // true/false value
+        //* compare(string, hashed value)
+        if(!passwordMatch) throw new Error('Email or Password does not match');
+        //4. After verified, provide a jwt(token)
+        const token = jwt.sign({id: user._id}, SECRET, {expiresIn: 60 * 60 * 24})
+        //5. Response status returned.
+        res.status (200).json({
+            message: "Successful",
+            user, token
+        })
+        
+    } catch (err) {
+       res.status(500).json({
+            ERROR: err.message
+       }) 
+    }
+})
 
 
 module.exports = router;
