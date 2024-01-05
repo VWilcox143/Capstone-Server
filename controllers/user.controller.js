d1176a926f60e87fb1883e4766f5a2941e7de190
AaronArredondo3 <Aaronarredondo63093@gmail.com>
2024-01-04 19:52:21 -0600

    User get all id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const User = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const expiresIn = {expiresIN: "1 day"};
+const expiresIn = {expiresIn: "1 day"};
 const SECRET = process.env.JWT;
 
 //! User Signup
@@ -34,6 +34,51 @@ router.post('/signup', async(req, res) => {
 });
 //!User Login
 
+
+//! Get All 
+
+router.get('/', async (req, res) =>{
+    try {
+
+        const allUsers = await User.find();
+        
+        // console.log(req.user);
+        if(allUsers.length === 0) throw new error
+
+        allUsers.length > 0?
+        res.status(200).json({
+            result: allUsers,
+        }) :
+        res.status(404).json({
+            result: 'No Users found'
+        })
+        
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        })
+    }
+});
+
+//! Get ONE 
+router.get('/find-one/:id', async (req, res) => {
+    try {
+        
+        const { id } = req.params;
+        const getUser = await User.findOne({_id: id}); 
+
+        if (!getUser) throw new Error ('no user found');
+
+        res.status(200).json({
+            results: getUser
+        })
+        
+    } catch (err) {
+        res.status(500).json({
+            error: err.message
+        })
+    }
+});
 
 
 module.exports = router;
