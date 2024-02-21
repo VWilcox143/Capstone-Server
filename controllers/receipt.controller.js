4b544a0c66e56e7bf4a81dbeed642b6eedae86cc
AaronArredondo3 <Aaronarredondo63093@gmail.com>
2024-02-21 17:21:45 -0600

    put in owner id in task controller.

--- controllers/receipt.controller.js
+++ controllers/receipt.controller.js
@@ -138,6 +138,28 @@ router.get('/type/:type', async (req, res) => {
 });
 
 //! Update One
+router.patch('/:id', validateSession, async(req, res) => {
+    try {
+        
+        const filter = {
+            _id: req.params.id,
+            owner_id: req.user._id
+        }
+
+        const info = req.body;
+
+        const returnOption = {new: true};
+
+        const updated = await Receipt.findOneAndUpdate(filter, info, returnOption);
+
+        res.status(200).json({
+            result: updated
+        })
+
+    } catch (err) {
+        errorHandling(res, err)
+    }
+})
 
 //! Delete one
 router.delete('/:id', validateSession, async (req, res) => {
